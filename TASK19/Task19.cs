e3ec50eac0c51a3b6d8ffe3c96f8e0192572ec2a
Denis_Gorbachev <mr.pluotttt@gmail.com>
2023-03-19 12:07:23 +0300

    finished

---
+++ TASK19/Task19.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 10000|| n > 99999)
+{
+Console.WriteLine("Вы ошиблись это не пятизначное число, введите снова");
+n = Convert.ToInt32(Console.ReadLine());
+}
+int a = n/10000;
+int b = (n%10000)/1000;
+int c = (n%100)/10;
+int d = n%10;
+if (a==d||c==d)
+    Console.Write("Число является полиномом");
+else
+    Console.Write("Число не является полиномом");
