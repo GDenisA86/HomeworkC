47d574946279f36e77a134f6d8ddeb65cf63f614
Denis_Gorbachev <mr.pluotttt@gmail.com>
2023-03-22 23:27:36 +0300

    not finished

---
+++ TASK36/Task36.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+void InputArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+}
+
+int schet(int[] array)
+{
+    int n = array[1];
+    for (int i = 0; i < array.Length; i=i+2)
+    {
+        n=n+array[i];
+    }
+    return n;
+}
+Console.Clear();
+Console.Write("введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray (array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Кол-во чётных чисел в массиве равно: {schet(array)}");
