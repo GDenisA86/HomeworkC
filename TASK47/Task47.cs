7618c63c03be8652114f546b0adf90c5b763f266
Denis_Gorbachev <mr.pluotttt@gmail.com>
2023-03-31 17:58:09 +0300

    final

--- TASK47/Task47.cs
+++ TASK47/Task47.cs
@@ -9,7 +9,7 @@ void InputMatrix(double[,] matrix)
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = Math.Round(new Random().NextDouble() * (100 + 100) - 100, 2);
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (10 + 10) - 10, 2);
     }
 }
 void PrintMatrix(double[,] matrix)
