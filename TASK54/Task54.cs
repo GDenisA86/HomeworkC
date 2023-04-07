30d18590f7b7f932f3638b38204e22a738a45401
Denis_Gorbachev <mr.pluotttt@gmail.com>
2023-04-07 22:23:30 +0300

    finished

---
+++ TASK54/Task54.cs
@@ -0,0 +1,57 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 99);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void Replace(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int c = 0; c < matrix.GetLength(1)-1; c++)
+            {
+                if (matrix[i, c] < matrix[i, c + 1])
+                {
+                    int max = matrix[i, c + 1];
+                    matrix[i, c + 1] = matrix[i, c];
+                    matrix[i, c] = max;
+                }
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры двумерного массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+Replace(matrix);
+PrintMatrix(matrix);
